821f151215160f44cd0c7ceb5800812a99ea1f41
James White <james@thewhites.co>
2022-12-23 11:25:32 -0600

    Use XHR request type "document"
    No need for the weight or hackiness of an iframe

--- src/include-html.js
+++ src/include-html.js
@@ -1,13 +1,4 @@
 class IncludeHtml extends HTMLElement {  
-  // This is wildly ironic, but in lieu of forcing users of this component to
-  // copy/paste four lines of boilerplate markup, I will compromise and render
-  // the template element from a javascript string. Blargh why isn't there a
-  // good way to do this >:(
-  static {
-    let template = '<template id="breathe-include-html"><iframe style="display none"></iframe><slot></slot></template>';
-    document.body.insertAdjacentHtml('afterbegin', template);
-  }
-
   // restrict action to valid values
   set action(value) {
     switch (value) {
@@ -22,28 +13,26 @@ class IncludeHtml extends HTMLElement {
 
   constructor() {
     super();
-    // properties
-    this.action = this.getAttribute('action');
-    this.selector = this.getAttribute('selector');
-    this.url = this.getAttribute('src');
-    // use the template
-    [this.iframe, this.slot] = template.content.cloneNode(true).children;
-    // mount the shadow dom and insert the iframe
-    this.shadow = this.attachShadow({mode: 'open'});
-    this.shadow.append(this.iframe);
   }
 
   connectedCallback() {
-    // register the callback
-    this.iframe.addEventListener('load', (function (event) {
-      let target = document.querySelector(this.selector);
-      let doc = this.contentDocument;
-      if (target)
-        for (let element of doc.body.children)
-          target[this.action](document.adoptNode(element));
-      this.shadow.append(this.slot);
-    }).bind(this));
+    this.action = this.getAttribute('action');
+    this.selector = this.getAttribute('selector');
+    this.url = this.getAttribute('src');
+    let self = this;
+    let req = new XMLHttpRequest();
+    req.responseType = "document";
+    req.onload = 
+    req.open("GET", "file.html");
+    req.send();
     // trigger the iframe load
     this.iframe.src = this.url;
   }
+
+  onresponse() {
+      let target = document.querySelector(self.selector);
+      if (target)
+        for (let el of req.response.body.children)
+          target[self.action](document.adoptNode(el));
+    };
 }

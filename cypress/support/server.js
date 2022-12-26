f8c5107b3086eb347fedff47d99d413ccf6c69d9
James White <james@thewhites.co>
2022-12-26 17:32:39 -0600

    Split html files out of the js

--- cypress/support/server.js
+++ cypress/support/server.js
@@ -1,55 +1,32 @@
-let http = require('http');
-let fs = require('fs');
+const http = require('node:http');
+const { readFile } = require('node:fs/promises');
 
-let index = `
-<html>
-  <head>
-    <title>Test page</title>
-  </head>
-  <body>
-    <h1>Html Includes</h1>
-    <p>about to include html as a document</p>
-    <include-html rel="loadme.html" action="before" selector="#the-anchor"></include-html>
-    <p>it should be loaded right before the next paragraph</p>
-    <p id="the-anchor">before this one</p>
-    <script src="include-html.js"></script>
-  </body>
-</html>
-`;
-let loadme = `
-<body>
-  <p>yep it loaded</p>
-  <script>
-    let el = document.createElement('i');
-    el.innerHtml = 'yep it loaded';
-    document.body.appendChild(el);
-  </script>
-</body>
-`;
+const IP = '127.0.0.1';
+const PORT = 3000;
 
-let server = http.createServer(function (req, res) {
-  switch (req.url.split('/').pop()) {
+const server = http.createServer();
+server
+  .on('request', async function (req, res) {
+    let file;
+    switch (req.url.split('/').pop()) {
     case 'index.html':
+      file = { path: 'cypress/support/index.html', mime: 'text/html' };
+      break;
+    case 'loadme.html':
+      file = { path: 'cypress/support/loadme.html', mime: 'text/html' };
+      break;
+    case 'include-html.js':
+      file = { path: 'src/include-html.js', mime: 'text/javascript' };
+      break;
+    default:
+      console.error(`'${req.url}' not found:\n${Object.keys(req).join('\n')}`);
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/plain');
+      return res.end('Not found');
+    }
+    let data = await readFile(file.path);
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end(index);
-    break;
-  case 'loadme.html':
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end(loadme);
-    break;
-  case 'include-html.js':
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/javascript');
-    fs.readFile('../../src/include-html.js', (data) => res.end(data));
-    break;
-  default:
-    console.error(`'${req.url}' not found:\n${Object.keys(req).join('\n')}`);
-    res.statusCode = 404;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Not found');
-  }
-});
-
-server.listen(3000, '127.0.0.1', () => console.log('Server running on :3000'));
+    res.setHeader('Content-Type', file.mime);
+    res.end(data);
+  })
+  .listen(PORT, IP, () => console.log('Server running on :3000'));
